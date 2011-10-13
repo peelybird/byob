bf3284868b1e249a5e29dad20434b89845c1e034
David Peel <david.peel@gmail.com>
2011-10-13 09:03:49 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Byob::Application.config.secret_token = '3bff3f91d47f836727b2fc75f649d0917448c14644e1bb74e0b9b2530e8f03ed12836d0597fb7ba99686f24044d8f82f5ae29d53e6f8b1297da2342cf27f0864'
